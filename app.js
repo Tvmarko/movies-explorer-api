64400b402b1b9a27d476a7d096571dd053605a11
Татьяна <tvmarko@yandex.ru>
2022-08-03 20:17:52 +0400

    4'

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const errorsHandler = require('./middlewares/error-handler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const cors = require('./middlewares/cors');
 
+const { MONGO_URL } = require('./utils/constants');
+
 const { PORT = 3000 } = process.env;
 
 const app = express();
@@ -15,7 +17,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://127.0.0.1:27017/moviesdb', {
+mongoose.connect(MONGO_URL, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
--- utils/constants.js
+++ utils/constants.js
@@ -5,6 +5,8 @@ const CONFLICT_ERROR = 409;
 const VORBIDDEN_ERROR = 403;
 const SERVER_ERROR = 500;
 
+const MONGO_URL = process.env.NODE_ENV === 'production' ? process.env.MONGO_URL : 'mongodb://127.0.0.1:27017/moviesdb';
+
 module.exports = {
   BADREQUEST_ERROR,
   NOTFOUND_ERROR,
@@ -12,4 +14,5 @@ module.exports = {
   CONFLICT_ERROR,
   VORBIDDEN_ERROR,
   SERVER_ERROR,
+  MONGO_URL,
 };
